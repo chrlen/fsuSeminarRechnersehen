c2a262c30f725677c7e967a544c144a75a3ce0f4
Christian Lengert <clengert@googlemail.com>
2017-06-18 20:24:23 +0200

    progress

---
+++ presentation/build.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+cd /home/me/git/fsuSeminarRechnersehen/presentation/
+sh clean.sh
+pdflatex pres.tex
+#pdflatex main.tex
+#rm *.out *.aux *.snm *.log
---
+++ presentation/clean.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+rm *.nav *.aux *.log *.toc *.out *.snm 
